1ea80f1df0e6377aa2bb5021e69ce672bf6939fb
meiguangya <15871737166@163.com>
2021-03-28 11:08:24 +0800

    kafka

---
+++ springboot-kafka/src/main/java/com/cool/KafkaConsumerTest.java
@@ -0,0 +1,57 @@
+package com.cool;
+
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+/**
+ * @author water33
+ */
+@SuppressWarnings("all")
+public class KafkaConsumerTest {
+
+    static Log log = LogFactory.get();
+
+    public static void main(String[] args) {
+        log.info("try to consumer...");
+        //创建用于连接kafka的properties
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test"); //消费组名称
+        props.setProperty("enable.auto.commit", "true"); //自动提交offset
+        props.setProperty("auto.commit.interval.ms", "1000"); //自动提交offset的时间间隔
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+
+        //创建consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        //指定消费者的topic
+        consumer.subscribe(Arrays.asList("eatMsg"));
+
+        //while循环不断拉取数据
+        while (true) {
+            //5秒超时 一次拉去一批数据
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(5));
+
+            for (ConsumerRecord<String, String> record : consumerRecords) {
+                String topic = record.topic();
+                long offset = record.offset();
+                String key = record.key();
+                String value = record.value();
+                log.info("topic:{},offset:{},key:{},value:{}",topic,offset,key,value);
+            }
+        }
+
+
+    }
+}
---
+++ springboot-kafka/src/main/java/com/cool/KafkaProducerTest.java
@@ -0,0 +1,65 @@
+package com.cool;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+
+/**
+ * @author water33
+ * 模拟生产者
+ */
+public class KafkaProducerTest {
+
+    static Log log = LogFactory.get();
+
+    public static void main(String[] args) {
+        //创建用于连接kafka的properties
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        //当生产者发送消息到Kafka中，以什么策略进行返回
+        props.setProperty("acks", "all");
+        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        //创建对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        //发送数据到topic中
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("eatMsg", "index", i+"");
+
+            kafkaProducer.send(record,(RecordMetadata recordMetadata, Exception exception)->{
+                if(exception==null){
+                    String topic = recordMetadata.topic();
+                    int partition = recordMetadata.partition();
+                    long offset = recordMetadata.offset();
+                    log.info("topic:{},partition:{},offset:{}",topic,partition,offset);
+                }else{
+                    exception.printStackTrace();
+                }
+            });
+
+
+//            Future<RecordMetadata> future = kafkaProducer.send(record);
+//            try {
+//                future.get();
+//                log.info("第{}条消息发送成功", i);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            }
+        }
+
+        //关闭
+        kafkaProducer.close();
+    }
+
+}
